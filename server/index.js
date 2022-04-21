ad047ab5720b40c7f0e565948f5f0847e39a4b24
Karlo Verde <karlo.verde.1996@gmail.com>
2022-04-21 04:01:55 -0500

    chore: log errors on reqs

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const logErrorOnRequest = require('./logger');
 
 const app = express();
 
 app.use((_, __, next) => next({ statusCode: 404, message: 'Not Found' }));
 
 // eslint-disable-next-line no-unused-vars
-app.use((err, _, res, __) => {
+app.use((err, req, res, __) => {
   const { statusCode, message } = err;
+  logErrorOnRequest(req, statusCode, message);
   res
     .status(statusCode || 500)
     .json({ message: message || 'Internal Server Error' });
