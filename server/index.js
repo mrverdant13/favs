075c66161a7edecc60c6045fcead2e7caa955cd0
Karlo Verde <karlo.verde.1996@gmail.com>
2022-04-21 03:12:50 -0500

    refactor: centralize error handling

--- server/index.js
+++ server/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((_, res) => res.status(404).json({ message: 'Not found' }));
-app.use((_, res) => res.status(500).json({ message: 'Internal server error' }));
+app.use((_, __, next) => next({ statusCode: 404, message: 'Not Found' }));
+app.use((err, _, res) => {
+  const { statusCode, message } = err;
+  res
+    .status(statusCode || 500)
+    .json({ message: message || 'Internal Server Error' });
+});
 
 module.exports = app;
