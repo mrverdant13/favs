b68c271e6aa7ef25accaad01e20d746ee3ac5adc
Karlo Verde <karlo.verde.1996@gmail.com>
2022-04-21 03:47:13 -0500

    refactor: use logger

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http = require('http');
-const app = require('./server');
 
+const app = require('./server');
+const logger = require('./logger');
 const { port } = require('./server/config');
 
 const server = http.createServer(app);
 server.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is running at port ${port}`);
+  logger.info(`Server listening on port ${port}`);
 });
