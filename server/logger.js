2230540e6a7aab23d1da09a5d24d2f96c5af0f85
Karlo Verde <karlo.verde.1996@gmail.com>
2022-04-21 04:10:22 -0500

    feat: log request ID

--- server/logger.js
+++ server/logger.js
@@ -2,7 +2,7 @@ const logger = require('../logger');
 
 function logErrorOnRequest(req, statusCode, message, level = 'error') {
   const date = new Date();
-  const metadata = `${req.ip} [${date.toISOString()}]`;
+  const metadata = `${req.ip} [${date.toISOString()}] ${req.id}`;
   const logMsg = `${metadata} - "${req.method} ${req.url}" - ${statusCode} - ${message}`;
   logger[level](logMsg);
 }
