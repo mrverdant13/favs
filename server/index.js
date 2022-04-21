bd001fd2f81a415881b8ec8f9a9af58ed1b29ff4
Karlo Verde <karlo.verde.1996@gmail.com>
2022-04-21 03:12:04 -0500

    feat: define behaviour for unexpected errors

--- server/index.js
+++ server/index.js
@@ -3,5 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use((_, res) => res.status(404).json({ message: 'Not found' }));
+app.use((_, res) => res.status(500).json({ message: 'Internal server error' }));
 
 module.exports = app;
