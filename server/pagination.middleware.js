10e6d4214903a8af4f9069504531793b03830e5f
Karlo Verde <karlo.verde.1996@gmail.com>
2022-04-22 01:44:55 -0500

    feat: validate pagination params

--- server/pagination.middleware.js
+++ server/pagination.middleware.js
@@ -2,9 +2,20 @@ const defaultLimit = 10;
 const maxLimit = 100;
 
 exports.parsePagination = (req, _, next) => {
-  const { limit: limitString, offset: offsetString } = req.query;
-  const limit = parseInt(limitString, 10) || defaultLimit;
-  const offset = parseInt(offsetString, 10) || 0;
+  const { limit: limitStr = defaultLimit, offset: offsetStr = 0 } = req.query;
+  const limit = parseInt(limitStr, 10);
+  const offset = parseInt(offsetStr, 10);
+  const limitIsValid = !Number.isNaN(limit) && limit > 0;
+  const offsetIsValid = !Number.isNaN(offset) && offset >= 0;
+  if (!limitIsValid || !offsetIsValid) {
+    next({
+      statusCode: 400,
+      message:
+        'Bad request: invalid pagination params.' +
+        `${limitIsValid ? '' : ' `limit` must be a positive integer.'}` +
+        `${offsetIsValid ? '' : ' `offset` must be a positive integer.'}`,
+    });
+  }
   req.limit = Math.min(limit, maxLimit);
   req.offset = offset;
   next();
