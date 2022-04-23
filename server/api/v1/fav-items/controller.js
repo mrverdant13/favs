d1d3a45310fc9d2a266a6cefb1ea3ba6db68c69c
Karlo Verde <karlo.verde.1996@gmail.com>
2022-04-23 02:50:16 -0500

    chore: implement fav items actions

---
+++ server/api/v1/fav-items/controller.js
@@ -0,0 +1,76 @@
+const { FavItem } = require('./fav-item.entity');
+
+// Middlewares
+
+/**
+ * Finds a fav item by the `id` path param and appends it to `req` object as `favItem`.
+ *
+ * If no fav item is found, returns the `404` HTTP code.
+ */
+exports.appendFavItemById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!id) res.status(400).json({ message: 'Missing `id` path param.' });
+    const favItemDoc = await FavItem.findById(id);
+    if (!favItemDoc) res.status(404).json({ message: 'Fav item not found' });
+    req.favItem = favItemDoc;
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Request handlers
+
+exports.createFavItem = async (req, res, next) => {
+  try {
+    const { body } = req;
+    const favItemModel = new FavItem(body);
+    const createdFavItemDoc = await favItemModel.save();
+    res.status(201).json(createdFavItemDoc);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.listFavItems = async (req, res, next) => {
+  try {
+    const { limit, offset, sortBy, direction } = req;
+    const [favItemsDocs, totalFavItems] = await Promise.all([
+      FavItem.find()
+        .sort({ [sortBy]: direction })
+        .skip(offset)
+        .limit(limit),
+      FavItem.countDocuments(),
+    ]);
+    res.status(200).json({ total: totalFavItems, favItems: favItemsDocs });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getFavItem = async (req, res) => {
+  const favItemDoc = req.favItem;
+  res.status(200).json(favItemDoc);
+};
+
+exports.editFavItem = async (req, res, next) => {
+  try {
+    const { favItem: favItemDoc, body: newFavItem } = req;
+    Object.assign(favItemDoc, newFavItem);
+    const updatedFavItemDoc = await favItemDoc.save();
+    res.status(200).json(updatedFavItemDoc);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.removeFavItem = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const removedFavItem = await FavItem.findByIdAndRemove(id);
+    res.status(200).json(removedFavItem);
+  } catch (err) {
+    next(err);
+  }
+};
