37629c29ccce627158885af73491718ed3f6815d
Karlo Verde <karlo.verde.1996@gmail.com>
2022-04-24 00:52:33 -0500

    feat: implement sign-up endpoint

---
+++ server/api/v1/users/controller.js
@@ -0,0 +1,14 @@
+const { User } = require('./user.entity');
+
+// Request handlers
+
+exports.signUp = async (req, res, next) => {
+  try {
+    const { body } = req;
+    const userModel = new User(body);
+    const createdUserDoc = await userModel.save();
+    res.status(201).json(createdUserDoc);
+  } catch (err) {
+    next(err);
+  }
+};
