a2785963dc0a4adcbd210e9ef5494183bb4b6d49
Karlo Verde <karlo.verde.1996@gmail.com>
2022-04-24 00:24:56 -0500

    feat: hidde sensitive data

--- server/api/v1/users/user.entity.js
+++ server/api/v1/users/user.entity.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
+const hiddenFields = {
+  password: {
+    type: String,
+    required: true,
+  },
+};
+
 const baseFields = {
   email: {
     type: String,
@@ -8,13 +15,18 @@ const baseFields = {
     trim: true,
     lowercase: true,
   },
-  password: {
-    type: String,
-    required: true,
-  },
+  ...hiddenFields,
 };
 
 const userSchema = new mongoose.Schema(baseFields, { timestamps: true });
+
+// eslint-disable-next-line func-names
+userSchema.methods.toJSON = function () {
+  const userJson = this.toObject();
+  const hiddenFieldNames = Object.keys(hiddenFields);
+  hiddenFieldNames.forEach((fieldName) => delete userJson[fieldName]);
+  return userJson;
+};
 
 const fields = Object.keys(userSchema.paths);
 
