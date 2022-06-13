2b08f48953da9454376cf9426666cad8f1f8a674
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2022-06-13 07:53:12 -0500

    docs: add online API docs (#8)

--- server/pagination.middleware.js
+++ server/pagination.middleware.js
@@ -1,12 +1,17 @@
 const defaultLimit = 10;
+const minLimit = 1;
 const maxLimit = 100;
 
+const defaultOffset = 0;
+const minOffset = 0;
+
 exports.parsePagination = (req, _, next) => {
-  const { limit: limitStr = defaultLimit, offset: offsetStr = 0 } = req.query;
+  const { limit: limitStr = defaultLimit, offset: offsetStr = defaultOffset } =
+    req.query;
   const limit = parseInt(limitStr, 10);
   const offset = parseInt(offsetStr, 10);
-  const limitIsValid = !Number.isNaN(limit) && limit > 0;
-  const offsetIsValid = !Number.isNaN(offset) && offset >= 0;
+  const limitIsValid = !Number.isNaN(limit) && limit >= minLimit;
+  const offsetIsValid = !Number.isNaN(offset) && offset >= minOffset;
   if (!limitIsValid || !offsetIsValid) {
     next({
       statusCode: 400,
@@ -20,3 +25,29 @@ exports.parsePagination = (req, _, next) => {
   req.offset = offset;
   next();
 };
+
+exports.paginatorQueryParamsDocs = [
+  {
+    in: 'query',
+    name: 'limit',
+    description: 'Number of items to return',
+    required: false,
+    schema: {
+      type: 'integer',
+      minimum: minLimit,
+      maximum: maxLimit,
+      default: defaultLimit,
+    },
+  },
+  {
+    in: 'query',
+    name: 'offset',
+    description: 'Number of items to skip',
+    required: false,
+    schema: {
+      type: 'integer',
+      minimum: minOffset,
+      default: defaultOffset,
+    },
+  },
+];
